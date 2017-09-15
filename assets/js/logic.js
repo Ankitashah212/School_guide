6de7874fcab1a56e1e2dc659846ef1bb1df30c8b
Larry Peteet <larrypv10@gmail.com>
2017-09-15 15:00:05 -0400

    Merged Charts.html but still cannot get data to show up in Bootstrap Table

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,20 +4,42 @@ ATN7AHDhDngU3Sb4EUtkVMaTkhUA1hr6dkDNro0A
 data.gov authentication key
 
 Query to select ethnicity for schools - i've kept year 2014, we can change year
-https://api.data.gov/ed/collegescorecard/v1/schools?fields=school.name,id,2014.student.demographics.race_ethnicity.white,2014.student.demographics.race_ethnicity.black,2014.student.demographics.race_ethnicity.hispanic,2014.student.demographics.race_ethnicity.asian,2014.student.demographics.race_ethnicity.aian,2014.student.demographics.race_ethnicity.nhpi,2014.student.demographics.race_ethnicity.two_or_more,2014.student.demographics.race_ethnicity.non_resident_alien,2014.student.demographics.race_ethnicity.unknown,2014.student.demographics.race_ethnicity.white_non_hispanic,2014.student.demographics.race_ethnicity.black_non_hispanic,2014.student.demographics.race_ethnicity.asian_pacific_islander&sort=2014.completion.rate_suppressed.overall:desc&api_key=ATN7AHDhDngU3Sb4EUtkVMaTkhUA1hr6dkDNro0A&school.name=new+york
+https://api.data.gov/ed/collegescorecard/v1/schools?fields=
+school.name,
+id,
+2014.student.demographics.race_ethnicity.white,
+2014.student.demographics.race_ethnicity.black,
+2014.student.demographics.race_ethnicity.hispanic,
+2014.student.demographics.race_ethnicity.asian,
+2014.student.demographics.race_ethnicity.aian,
+2014.student.demographics.race_ethnicity.nhpi,
+2014.student.demographics.race_ethnicity.two_or_more,
+2014.student.demographics.race_ethnicity.non_resident_alien,
+2014.student.demographics.race_ethnicity.unknown,
+2014.student.demographics.race_ethnicity.white_non_hispanic,
+2014.student.demographics.race_ethnicity.black_non_hispanic,
+2014.student.demographics.race_ethnicity.asian_pacific_islander&
+sort=2014.completion.rate_suppressed.overall:desc&
+api_key=ATN7AHDhDngU3Sb4EUtkVMaTkhUA1hr6dkDNro0A&
+school.name=new+york
 
 
 //the whole school object --- lot of data
-https://api.data.gov/ed/collegescorecard/v1/schools?id=164924&api_key=ATN7AHDhDngU3Sb4EUtkVMaTkhUA1hr6dkDNro0A
+https://api.data.gov/ed/collegescorecard/v1/schools?id=164924&
+api_key=ATN7AHDhDngU3Sb4EUtkVMaTkhUA1hr6dkDNro0A
 
 
 // student financial data - not sure how I want to use it, at this point just ignore
-https://api.data.gov/ed/collegescorecard/v1/schools?fields=school.name,id,2012.aid.median_debt.completers.overall,2012.repayment.1_yr_repayment.completers,2012.earnings.10_yrs_after_entry.working_not_enrolled.mean_earnings&page=100&api_key=ATN7AHDhDngU3Sb4EUtkVMaTkhUA1hr6dkDNro0A
+https://api.data.gov/ed/collegescorecard/v1/schools?fields=school.name,id,2012.aid.median_debt.completers.overall,2012.repayment.1_yr_repayment.completers,2012.earnings.10_yrs_after_entry.working_not_enrolled.mean_earnings&
+page=100&
+api_key=ATN7AHDhDngU3Sb4EUtkVMaTkhUA1hr6dkDNro0A
 
 // my primary focus - gets overall sat score for school
-https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&_fields=id,school.name,2013.student.size,
+https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&
+_fields=id,school.name,2013.student.size,
 2013.admissions.sat_scores.average.overall
-&api_key=ATN7AHDhDngU3Sb4EUtkVMaTkhUA1hr6dkDNro0A&school.name=university+texas
+&api_key=ATN7AHDhDngU3Sb4EUtkVMaTkhUA1hr6dkDNro0A&
+school.name=university+texas
 
 The result fetches metadata and result array. Metadata says how many total results. by default  always page 0
 you can add page count as part of query
@@ -25,11 +47,25 @@ you can add page count as part of query
 
 $(document).ready(function() {
 
+    //*******************************************/
+    //****************  EVENTS  *****************/
+    //*******************************************/
 
+    //Input Form Submit Button calls fetchData() Function
     jQuery("form").submit(fetchData);
 
-    function fetchData(event) {
+    //Example Calling NamedFunction on Button Click:
+    //$("#selector").on("click", namedFunction);
 
+    //DrawChart Button calls displayPieChart() Function
+    jQuery("#drawChart").on("click", displayPieChart);
+
+    //******************************************/
+    //**************  FUNCTIONS  ***************/
+    //******************************************/
+
+    function fetchData(event) {
+        
         console.log("fetchData() Called")
         event.preventDefault();
         
@@ -41,31 +77,53 @@ $(document).ready(function() {
         // &_page=1 for page 1 and so forth by default page 0
         
         var query = "https://api.data.gov/ed/collegescorecard/v1/schools.json?"
-            + "school.degrees_awarded.predominant=2,3&_fields=id,school.name,"
-            + "2013.student.size,2013.admissions.sat_scores.average.overall"
-            + "&api_key=ATN7AHDhDngU3Sb4EUtkVMaTkhUA1hr6dkDNro0A&"
-            + "school.name=" + schoolName;
+            + "school.degrees_awarded.predominant=2,3&" +
+            "_fields=id," +
+            "school.name,"
+            + "2013.student.size," +
+            "2013.admissions.sat_scores.average.overall&" +
+            "api_key=ATN7AHDhDngU3Sb4EUtkVMaTkhUA1hr6dkDNro0A&"+
+            "school.name=" + schoolName;
 
+        //DEBUG: TRYING WEB EXAMPLE!!
+
+        $.ajax({
+            "url": query,
+            "done": function(json) {
+                var tableHeaders;
+                $.each(json.columns, function(i, val){
+                    tableHeaders += "<th>" + val + "</th>";
+                });
+                 
+                $("#tableDiv").empty();
+                $("#tableDiv").append('<table id="displayTable" class="display" cellspacing="0" width="100%"><thead><tr>' + tableHeaders + '</tr></thead></table>');
+                //$("#tableDiv").find("table thead tr").append(tableHeaders);  
+                 
+                $('#displayTable').dataTable(json);
+            },
+            "dataType": "json"
+        });
+    
         $.ajax({
             url: query,
             method: 'GET',
         //}).done(function (result) {
-        }).success(function (result) {
+        }).done(function (result) {
             // console.log(query);
             console.log(result);
             console.log(result.results["0"]["school.name"]);
             displayData(result);
             //LARRYP: The code below should work, tried data: result, result.results, result.results[0] and each time it says "No Data" even though we have data
-//            $('#clienti').bootstrapTable({
+            //$('#clienti').bootstrapTable({
                 // data: result.results[0]
                 //data: result.results,
                 //data: result,
                 //rows: 1
-//                load: result.results
-//            });
-         });// end of Ajax call
-    } // end of function
-
+                //load: result.results
+            //});
+        });// end of Ajax call
+    } // fetchData()
+        
     function displayData(result) {
         // Not Currently Used (Example from Web)
         var mydata = [
@@ -93,8 +151,8 @@ $(document).ready(function() {
                 "Mobile":"00112233445566",
                 "Note":"Vediamo se funziona questo"
             }
-        ];
- 
+        ]; //mydata
+    
         console.log("Inside displayData()");
         /* result.results[0]["2013.admissions.sat_scores.average.overall"]  */
         console.log("result", result);
@@ -122,6 +180,137 @@ $(document).ready(function() {
             data: mydata2
         });
     
-    }
+    } //displayData()
 
-}) //$(document).ready(function() {
+    function displayPieChart(event) {
+
+        console.log("event", event);
+        var schoolName = $("#school").val().trim();
+        schoolName = schoolName.replace(' ', '+');
+        if (schoolName == "") {
+            alert("Please Enter a School Name.");
+            return;
+        }
+        
+        var query = "https://api.data.gov/ed/collegescorecard/v1/schools?fields=school.name," +
+            "id," +
+            "2014.student.demographics.race_ethnicity.white," +
+            "2014.student.demographics.race_ethnicity.black," +
+            "2014.student.demographics.race_ethnicity.hispanic," +
+            "2014.student.demographics.race_ethnicity.asian," +
+            "2014.student.demographics.race_ethnicity.aian," +
+            "2014.student.demographics.race_ethnicity.nhpi," +
+            "2014.student.demographics.race_ethnicity.two_or_more," +
+            "2014.student.demographics.race_ethnicity.non_resident_alien," +
+            "2014.student.demographics.race_ethnicity.unknown," +
+            "2014.student.demographics.race_ethnicity.white_non_hispanic," +
+            "2014.student.demographics.race_ethnicity.black_non_hispanic," +
+            "2014.student.demographics.race_ethnicity.asian_pacific_islander&" +
+            "sort=2014.completion.rate_suppressed.overall:desc&" +
+            "api_key=ATN7AHDhDngU3Sb4EUtkVMaTkhUA1hr6dkDNro0A&" +
+            //"school.name=university+texas+austin"
+            "school.name=" + schoolName
+
+        console.log("query", query);
+
+        var asian, black, white, hispanic, nRAlien, others;
+        
+        $.ajax({
+            url: query,
+            method: 'GET',
+        }).done(function (school) {
+            // console.log(query);
+            console.log(school);
+            white = school.results["0"]["2014.student.demographics.race_ethnicity.white"];
+            nRAlien = (school.results["0"]["2014.student.demographics.race_ethnicity.non_resident_alien"]);
+            hispanic = (school.results["0"]["2014.student.demographics.race_ethnicity.hispanic"]);
+            black = (school.results["0"]["2014.student.demographics.race_ethnicity.black"]);
+            asian = (school.results["0"]["2014.student.demographics.race_ethnicity.asian"]);
+            others = 1 - (white + black + hispanic + asian + nRAlien);
+            console.log(white, nRAlien, hispanic, black, asian, others);
+            var ctx = document.getElementById("pieChart");
+            console.log("white, nRAlien, hispanic, black, asian, others");
+    
+            console.log(white, nRAlien, hispanic, black, asian, others);
+    
+            //data fot pie chart
+            data = {
+                datasets: [{
+                    backgroundColor: [
+                        //set colors for chart
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    //data for pie
+                    data: [white, black, asian, hispanic, nRAlien, others]
+                }],
+    
+                // These labels appear in the legend and in the tooltips when hovering different arcs
+                labels: [
+                    'Whilte',
+                    'Black',
+                    'Asian',
+                    'Hispanic',
+                    'Non Resident Alien',
+                    'Others'
+                ]
+            }; //data
+
+            new Chart(ctx, {
+                data: data,
+                type: 'pie',
+            });
+    
+            // bar part, i'll let the team decide on which one to use
+            // Bar chart
+            /*
+                var cht = document.getElementById("barChart");
+                //data fot bar chart
+                data = {
+                    datasets: [{
+                        backgroundColor: [
+                            //set colors for chart
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        //data for bar
+                        data: [0.5, 0.6, 0.2, 0.1, 1.1, 0.8]
+                    }],
+    
+                    // These labels appear in the legend and in the tooltips when hovering different arcs
+                    labels: [
+                        'Whilte',
+                        'Black',
+                        'Asian',
+                        'pacific',
+                        'native American',
+                        'others'
+                    ]
+                };
+                new Chart(ctx, {
+                    data: data,
+                    type: 'bar',
+                });
+                
+            */
+        });// end of Ajax call
+            
+    }; //displayPieChart
+
+
+    //******************************************/
+    //**************  MAIN CODE  ***************/
+    //******************************************/
+
+    //No Main Code Yet!
+  
+
+}) ///$(document).ready(function() {
