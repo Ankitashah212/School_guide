11d1935df8a40baa58af873afd7d7a8cf0e970c7
Kita <ankitashah212@gmail.com>
2017-09-20 14:45:31 -0400

    map showing an info window with school adress derived from lat lng

--- assets/js/location.js
+++ assets/js/location.js
@@ -1,14 +1,31 @@
 //******************************************/
 //**************  FUNCTIONS  ***************/
 //******************************************/
-
-
 function initMap() {
+//set location as lat and lng from school object
   var myCenter = new google.maps.LatLng(gLat, gLong);
   var mapCanvas = document.getElementById("googleMap");
   var mapOptions = { center: myCenter, zoom: 10 };
   var map = new google.maps.Map(mapCanvas, mapOptions);
-  var marker = new google.maps.Marker({ position: myCenter });
-  marker.setMap(map);
-}
+  var geocoder = new google.maps.Geocoder;
+  var infowindow = new google.maps.InfoWindow;
 
+  geocodeLatLng(geocoder, map, infowindow, myCenter);
+}
+// function to get the map an actual address reverse geocoded from lat a=lng
+function geocodeLatLng(geocoder, map, infowindow, myCenter) {
+  
+  geocoder.geocode({ 'location': myCenter }, function (results, status) {
+    if (status === 'OK') {
+      if (results[0]) {
+        map.setZoom(11);
+        var marker = new google.maps.Marker({
+          position: myCenter,
+          map: map
+        });
+        infowindow.setContent(results[0].formatted_address);
+        infowindow.open(map, marker);
+      }
+    }
+  });
+}
